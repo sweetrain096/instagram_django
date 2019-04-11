a232bd3954087d1018923b3a6f722363d4762f62
sweetrain096 <gpfhddl09@gmail.com>
2019-04-11 02:15:08 +0000

    Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    
+    def __str__(self):
+        return f'Post : {self.pk} - {self.content}'
+    
+    def get_absolute_url(self):
+        return reverse('posts:detail', args=[self.pk])
+        # reverse : object가 가야하는데 뒤의 내용을 실제 path로 만드는 역할.
