c9757bf7f0f19ba0196b4ea5657672dac745774c
sweetrain096 <gpfhddl09@gmail.com>
2019-04-11 01:45:19 +0000

    default settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(^tybm^y3s6jzbiyvb0c^22#o$k+g7^)#!diy!rwmp%9vwwa0t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
