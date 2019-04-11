394e2056077b0befd46873089568ac8ee1f53eed
sweetrain096 <gpfhddl09@gmail.com>
2019-04-11 06:49:46 +0000

    post CRUD|list꾸미기

--- posts/urls.py
+++ posts/urls.py
@@ -5,4 +5,8 @@ app_name = 'posts'
 
 urlpatterns = [
     path('', views.list, name="list"),
+    path('create/', views.create, name="create"),
+    path('detail/<int:post_pk>/', views.detail, name="detail"),
+    path('edit/<int:post_pk>/', views.edit, name="edit"),
+    path('delete/<int:post_pk>/', views.delete, name="delete"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def list(request):
-    return render(request, 'posts/list.html')
+    posts = Post.objects.all()
+    
+    context = {'posts' : posts}
+    return render(request, 'posts/list.html', context)
+    
+def create(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+    else:
+        post_form = PostForm()
+    context = {'post_form' : post_form}
+    
+    return render(request, 'posts/form.html', context)
+    
+def detail(request, post_pk):
+    post = Post.objects.get(pk=post_pk)
+    context = {'post' : post}
+    return render(request, 'posts/detail.html', context)
+    
+def edit(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == 'POST':
+        post_form = PostForm(request.POST, instance=post)
+        if post_form.is_valid():
+            post_form.save()
+    else:
+        post_form = PostForm(instance=post)
+    context = {'post_form' : post_form}
+        
+    return render(request, 'posts/form.html', context)
+    
+def delete(request, post_pk):
+    post = get_object_or_404(Post, pk=post_pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('posts:list')
+    else:
+        return redirect('posts:detail', post_pk)
