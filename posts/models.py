67ca45abcbfbc4c5c7760826c8d48cd403ae3cba
sweetrain096 <gpfhddl09@gmail.com>
2019-04-12 02:34:41 +0000

    Post - image 필드 추가

--- posts/models.py
+++ posts/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 # Create your models here.
 class Post(models.Model):
     content = models.TextField()
-
+    image = models.ImageField()
     
     def __str__(self):
         return f'Post : {self.pk} - {self.content}'
